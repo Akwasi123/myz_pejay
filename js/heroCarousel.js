e35a4b1d73056741a84e49baa0694a2271ef0370
Akwasi Asante-Krobea <49639685+Akwasi123@users.noreply.github.com>
2021-11-25 19:01:53 +0000

    completed hero and intro sections

---
+++ js/heroCarousel.js
@@ -0,0 +1,13 @@
+// swiper js carousel
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    // direction: 'vertical',
+    loop: true,
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+    },
+    effect: 'fade',  
+});
+  
