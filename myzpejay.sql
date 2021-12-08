7404a768f3a8bc5c4213afc0b86e874df1ba0627
Akwasi123 <akwasi.asantekrobea1@gmail.com>
2021-12-08 16:39:09 +0000

    database updated

--- myzpejay.sql
+++ myzpejay.sql
@@ -16,9 +16,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
-Drop schema if exists myzpejay;
-create schema myzpejay;
-use myzpejay;
+
 --
 -- Database: `myzpejay`
 --
